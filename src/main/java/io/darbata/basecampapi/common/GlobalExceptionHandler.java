2a4049e5ab24d4d2417d281c4c484d999b40e6f9
Darren Batarina <batarina.darren@gmail.com>
2026-03-07 00:00:26 +1100

    handle all exceptions

--- src/main/java/io/darbata/basecampapi/common/GlobalExceptionHandler.java
+++ src/main/java/io/darbata/basecampapi/common/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package io.darbata.basecampapi.common;
 
-import io.darbata.basecampapi.github.NoTokenException;
+import io.darbata.basecampapi.cloud.InvalidPresignRequestException;
+import io.darbata.basecampapi.github.*;
+import io.darbata.basecampapi.projects.AssignmentIssueException;
 import org.junit.jupiter.api.Order;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AssignmentIssueException.class)
+    public ResponseEntity<AssignmentIssueException> assignmentIssueException(AssignmentIssueException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex);
+    }
 
     @ExceptionHandler(NoTokenException.class)
     public ResponseEntity<Object> handleNoTokenException(NoTokenException ex) {
@@ -24,6 +31,36 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidUploadTypeException.class)
     public ResponseEntity<Object> handleInvalidUploadTypeException(InvalidUploadTypeException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidPresignRequestException.class)
+    public ResponseEntity<Object> handleInvalidPresignRequestException(InvalidPresignRequestException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ExpiredAccessTokenException.class)
+    public ResponseEntity<Object> handleExpiredAccessTokenException(ExpiredAccessTokenException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(TokenGenerationException.class)
+    public ResponseEntity<Object> handleTokenGenerationException(TokenGenerationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NoGithubTokenException.class)
+    public ResponseEntity<Object> handleNoGithubTokenException(NoGithubTokenException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(GithubCodeTokenExchangeException.class)
+    public ResponseEntity<Object> handleGithubCodeTokenExchangeException(GithubCodeTokenExchangeException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ExpiredRefreshTokenException.class)
+    public ResponseEntity<Object> handleExpiredRefreshTokenException(ExpiredRefreshTokenException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
